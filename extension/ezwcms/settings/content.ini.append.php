5e5ac93008c5d9f231591aa84d4da0e72c38bc42
Lukasz Serwatka <ls@ez.no>
2006-10-26 08:35:10 +0000

    - INI settings update
    git-svn-id: http://svn.ez.no/svn/extensions/ezwcms/trunk@417 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- extension/ezwcms/settings/content.ini.append.php
+++ extension/ezwcms/settings/content.ini.append.php
@@ -1,57 +1,5 @@
 <?php /*
 
-[object]
-AvailableClasses[]=itemized_sub_items
-AvailableClasses[]=itemized_subtree_items
-AvailableClasses[]=highlighted_object
-# AvailableClasses[]=products
-# AvailableClasses[]=listwithintrohoriz
-AvailableClasses[]=vertically_listed_sub_items
-AvailableClasses[]=horizontally_listed_sub_items
-
-ClassDescription[itemized_sub_items]=Itemized Sub Items
-ClassDescription[itemized_subtree_items]=Itemized Subtree Items
-ClassDescription[highlighted_object]=Highlighted Object
-ClassDescription[vertically_listed_sub_items]=Vertically Listed Sub Items
-ClassDescription[horizontally_listed_sub_items]=Horizontally Listed Sub Items
-
-[embed]
-AvailableClasses[]=itemized_sub_items
-AvailableClasses[]=itemized_subtree_items
-AvailableClasses[]=highlighted_object
-# AvailableClasses[]=products
-AvailableClasses[]=vertically_listed_sub_items
-# AvailableClasses[]=listwithintrohoriz
-AvailableClasses[]=horizontally_listed_sub_items
-
-ClassDescription[itemized_sub_items]=Itemized Sub Items
-ClassDescription[itemized_subtree_items]=Itemized Subtree Items
-ClassDescription[highlighted_object]=Highlighted Object
-ClassDescription[vertically_listed_sub_items]=Vertically Listed Sub Items
-ClassDescription[horizontally_listed_sub_items]=Horizontally Listed Sub Items
-
-[table]
-AvailableClasses[]=list
-AvailableClasses[]=cols
-AvailableClasses[]=comparison
-AvailableClasses[]=default
-
-ClassDescription[list]=List
-ClassDescription[cols]=Timetable
-ClassDescription[comparison]=Comparison Table
-ClassDescription[default]=Default
-
-[factbox]
-CustomAttributes[]=align
-CustomAttributes[]=title
-CustomAttributesDefaults[align]=right
-CustomAttributesDefaults[title]=factbox
-
-[quote]
-CustomAttributes[]=align
-CustomAttributesDefaults[align]=right
-
-
 [folder]
 SummaryInFullView=enabled
 
