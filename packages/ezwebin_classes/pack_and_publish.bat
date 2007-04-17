839312e6674a06bcf3b436353ae45c0bebded5bf
Lukasz Serwatka <ls@ez.no>
2007-04-17 06:24:24 +0000

    - Removed old content classes
    git-svn-id: http://svn.ez.no/svn/extensions/ezwebin/trunk@1222 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- packages/ezwebin_classes/pack_and_publish.bat
+++
@@ -1,3 +0,0 @@
-del /Q ezwebin_classes.ezpkg
-C:\Progra~1\7-Zip\7z.exe a -r -ttar -x!*.cache -x!*.svn -x!pack_and_publish.bat ezwebin_classes.ezpkg
-move /Y ezwebin_classes.ezpkg c:\www\packages.ez.no\ezpublish\3.9.1.rc1\
