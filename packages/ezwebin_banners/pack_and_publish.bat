a22485cc310b3374b065986be0e9ca0765bf9875
Lukasz Serwatka <ls@ez.no>
2007-04-17 06:18:40 +0000

    - Removed old demo content
    git-svn-id: http://svn.ez.no/svn/extensions/ezwebin/trunk@1218 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- packages/ezwebin_banners/pack_and_publish.bat
+++
@@ -1,3 +0,0 @@
-del /Q ezwebin_banners.ezpkg
-C:\Progra~1\7-Zip\7z.exe a -r -ttar -x!*.cache -x!*.svn -x!pack_and_publish.bat ezwebin_banners.ezpkg
-move /Y ezwebin_banners.ezpkg c:\www\packages.ez.no\ezpublish\3.9.1rc1\
