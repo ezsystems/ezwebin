30f452168545c4fb66973060da9b31736bb04ee9
Lukasz Serwatka <ls@ez.no>
2007-04-25 14:12:28 +0000

    - Updated versions
    git-svn-id: http://svn.ez.no/svn/extensions/ezwebin/trunk@1303 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- packages/ezwebin_extension/ezextension/ezwebin/ezinfo.php
+++ packages/ezwebin_extension/ezextension/ezwebin/ezinfo.php
@@ -27,8 +27,8 @@ class ezwebinInfo
     function info()
     {
         return array( 'Name' => "eZ Publish Website Interface",
-                      'Version' => "1.1.0",
-                      'Copyright' => "Copyright (C) 1999-2006 eZ systems AS",
+                      'Version' => "1.2.0",
+                      'Copyright' => "Copyright (C) 1999-2007 eZ systems AS",
                       'License' => "GNU General Public License v2.0"
                      );
     }
