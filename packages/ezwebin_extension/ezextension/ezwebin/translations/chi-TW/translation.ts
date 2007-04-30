10efae285e65936bde75105bf26bf575fb282340
Ole Marius Smestad <oms@ez.no>
2007-04-30 12:01:24 +0000

    - Fixed language attribute added in 1.1 version of TS format to correct language.
    git-svn-id: http://svn.ez.no/svn/extensions/ezwebin/trunk@1359 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- packages/ezwebin_extension/ezextension/ezwebin/translations/chi-TW/translation.ts
+++ packages/ezwebin_extension/ezextension/ezwebin/translations/chi-TW/translation.ts
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="en_GB">
+<!DOCTYPE TS><TS version="1.1" language="zh_TW">
 <context>
     <name>design/ezwebin/article/article_index</name>
     <message>
