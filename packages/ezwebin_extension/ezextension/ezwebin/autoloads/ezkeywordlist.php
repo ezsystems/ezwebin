afd19a293a4a9a5ad25d38e30d8bb9b51c43286d
Lukasz Serwatka <ls@ez.no>
2008-05-19 10:24:04 +0000

    - Added enhancement #013051: Add language priority check and permission filter to ezkeywordlist tpl operator   (Merged from trunk (1.4-0) rev. 2913)
    git-svn-id: http://svn.ez.no/svn/extensions/ezwebin/stable/1.3@2914 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- packages/ezwebin_extension/ezextension/ezwebin/autoloads/ezkeywordlist.php
+++ packages/ezwebin_extension/ezextension/ezwebin/autoloads/ezkeywordlist.php
@@ -37,19 +37,45 @@ class eZKeywordList
             {
                 include_once( 'lib/ezdb/classes/ezdb.php' );
                 $db = eZDB::instance();
+                
+                if( $parentNodeID )
+                {
+                    $node = eZContentObjectTreeNode::fetch( $parentNodeID );
+                    if ( $node )
+                        $pathString = "AND ezcontentobject_tree.path_string like '" . $node->attribute( 'path_string' ) . "%'";
+                    $parentNodeIDSQL = "AND ezcontentobject_tree.node_id != " . (int)$parentNodeID;
+                }
+
+                $showInvisibleNodesCond = eZContentObjectTreeNode::createShowInvisibleSQLString( true, false );
+                $limitation = false;
+                $limitationList = eZContentObjectTreeNode::getLimitationList( $limitation );
+                $sqlPermissionChecking = eZContentObjectTreeNode::createPermissionCheckingSQL( $limitationList );
+
+                $versionNameJoins = " AND ezcontentobject_tree.contentobject_id = ezcontentobject_name.contentobject_id AND
+                                            ezcontentobject_tree.contentobject_version = ezcontentobject_name.content_version AND ";
+                $languageFilter = " AND " . eZContentLanguage::languagesSQLFilter( 'ezcontentobject' );
+                $versionNameJoins .= eZContentLanguage::sqlFilter( 'ezcontentobject_name', 'ezcontentobject' );
 
                 $rs = $db->arrayQuery( "SELECT DISTINCT ezkeyword.keyword
                                             FROM ezkeyword_attribute_link,
                                                  ezkeyword,
+                                                 ezcontentobject,
+                                                 ezcontentobject_name,
                                                  ezcontentobject_attribute,
                                                  ezcontentobject_tree,
                                                  ezcontentclass
+                                                 $sqlPermissionChecking[from]
                                             WHERE ezkeyword.id = ezkeyword_attribute_link.keyword_id
                                                 AND ezkeyword_attribute_link.objectattribute_id = ezcontentobject_attribute.id
                                                 AND ezcontentobject_tree.contentobject_id = ezcontentobject_attribute.contentobject_id
                                                 AND ezkeyword.class_id = ezcontentclass.id
-                                                AND ezcontentobject_tree.parent_node_id = " . (int)$parentNodeID . "
                                                 AND ezcontentclass.identifier = '" . $classIdentifier . "'
+                                                $pathString
+                                                $parentNodeIDSQL
+                                                $showInvisibleNodesCond
+                                                $sqlPermissionChecking[where]
+                                                $languageFilter
+                                                $versionNameJoins
                                             ORDER BY ezkeyword.keyword ASC" );
                 $operatorValue = $rs;
             } break;
