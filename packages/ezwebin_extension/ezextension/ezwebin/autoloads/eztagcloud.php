4a06e756802b3bdf3a7188887a6ffdb20fd4b4db
Lukasz Serwatka <ls@ez.no>
2008-09-22 07:13:26 +0000

    - Performance optimization in eztagcloud tpl operator
    git-svn-id: http://svn.ez.no/svn/extensions/ezwebin/trunk@3218 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- packages/ezwebin_extension/ezextension/ezwebin/autoloads/eztagcloud.php
+++ packages/ezwebin_extension/ezextension/ezwebin/autoloads/eztagcloud.php
@@ -19,8 +19,8 @@ class eZTagCloud
     function namedParameterList()
     {
         return array( 'eztagcloud' => array( 'params' => array( 'type' => 'array',
-        'required' => false,
-        'default' => array() ) ) );
+                        'required' => false,
+                        'default' => array() ) ) );
     }
 
     function modify( $tpl, $operatorName, $operatorParameters, &$rootNamespace, &$currentNamespace, &$operatorValue, &$namedParameters )
@@ -28,121 +28,105 @@ class eZTagCloud
         switch ( $operatorName )
         {
             case 'eztagcloud':
+            {
+
+                $tags = array();
+                $tagCloud = array();
+                $parentNodeID = 0;
+                $classIdentifier = '';
+                $classIdentifierSQL = '';
+                $pathString = '';
+                $parentNodeIDSQL = '';
+
+                if ( isset( $namedParameters['params']['class_identifier'] ) )
+                    $classIdentifier = $namedParameters['params']['class_identifier'];
+
+                if ( isset( $namedParameters['params']['parent_node_id'] ) )
+                    $parentNodeID = $namedParameters['params']['parent_node_id'];
+
+                $db = eZDB::instance();
+
+                if( $classIdentifier )
                 {
+                    $classID = eZContentClass::classIDByIdentifier( $classIdentifier );
+                    $classIdentifierSQL = "AND ezcontentobject.contentclass_id = '" . $classID . "'";
+                }
 
-                    $tags = array();
-                    $tagCloud = array();
-                    $parentNodeID = 0;
-                    $classID = '';
-                    $classIdentifier = '';
-                    $classIdentifierSQL = '';
-                    $pathString = '';
-                    $parentNodeIDSQL = '';
+                if( $parentNodeID )
+                {
+                    $node = eZContentObjectTreeNode::fetch( $parentNodeID );
+                    if ( $node )
+                        $pathString = "AND ezcontentobject_tree.path_string like '" . $node->attribute( 'path_string' ) . "%'";
+                    $parentNodeIDSQL = 'AND ezcontentobject_tree.node_id != ' . (int)$parentNodeID;
+                }
 
-                    if ( isset( $namedParameters['params']['class_identifier'] ) )
-                        $classIdentifier = $namedParameters['params']['class_identifier'];
+                $showInvisibleNodesCond = eZContentObjectTreeNode::createShowInvisibleSQLString( true, false );
+                $limitation = false;
+                $limitationList = eZContentObjectTreeNode::getLimitationList( $limitation );
+                $sqlPermissionChecking = eZContentObjectTreeNode::createPermissionCheckingSQL( $limitationList );
 
-                    if ( isset( $namedParameters['params']['parent_node_id'] ) )
-                        $parentNodeID = $namedParameters['params']['parent_node_id'];
+                $languageFilter = 'AND ' . eZContentLanguage::languagesSQLFilter( 'ezcontentobject' );
 
-                    include_once( 'lib/ezdb/classes/ezdb.php' );
-                    $db = eZDB::instance();
+                $rs = $db->arrayQuery( "SELECT ezkeyword.keyword, count(*) as keyword_count
+                                        FROM ezkeyword,
+                                            ezkeyword_attribute_link,
+                                            ezcontentobject,
+                                            ezcontentobject_attribute,
+                                            ezcontentobject_tree
+                                            $sqlPermissionChecking[from]
+                                        WHERE ezkeyword.id = ezkeyword_attribute_link.keyword_id
+                                            AND ezkeyword_attribute_link.objectattribute_id = ezcontentobject_attribute.id
+                                            AND ezcontentobject_attribute.contentobject_id = ezcontentobject_tree.contentobject_id
+                                            AND ezcontentobject_attribute.contentobject_id = ezcontentobject.id
+                                            AND ezcontentobject.status = ".eZContentObject::STATUS_PUBLISHED."
+                                            AND ezcontentobject_attribute.version = ezcontentobject.current_version
+                                            AND ezcontentobject_tree.main_node_id = ezcontentobject_tree.node_id
+                                            $pathString
+                                            $parentNodeIDSQL
+                                            $classIdentifierSQL
+                                            $showInvisibleNodesCond
+                                            $sqlPermissionChecking[where]
+                                            $languageFilter
+                						GROUP BY ezkeyword.id
+                                        ORDER BY ezkeyword.keyword ASC" );
 
-                    if( $classIdentifier )
-                    {
-                        $class = eZContentClass::fetchByIdentifier( $classIdentifier );
-                        if ( $class )
-                            $classID = $class->attribute( 'id' );
-                        $classIdentifierSQL = "AND ezcontentclass.identifier = '" . $classIdentifier . "'";
-                    }
+                foreach( $rs as $row )
+                {
+                    $tags[$row['keyword']] = $row['keyword_count'];
+                }
 
-                    if( $parentNodeID )
-                    {
-                        $node = eZContentObjectTreeNode::fetch( $parentNodeID );
-                        if ( $node )
-                            $pathString = "AND ezcontentobject_tree.path_string like '" . $node->attribute( 'path_string' ) . "%'";
-                        $parentNodeIDSQL = "AND ezcontentobject_tree.node_id != " . (int)$parentNodeID;
-                    }
+                $maxFontSize = 200;
+                $minFontSize = 100;
 
-                    $showInvisibleNodesCond = eZContentObjectTreeNode::createShowInvisibleSQLString( true, false );
-                    $limitation = false;
-                    $limitationList = eZContentObjectTreeNode::getLimitationList( $limitation );
-                    $sqlPermissionChecking = eZContentObjectTreeNode::createPermissionCheckingSQL( $limitationList );
+                $maxCount = 0;
+                $minCount = 0;
 
-                    $versionNameJoins = " AND ezcontentobject_tree.contentobject_id = ezcontentobject_name.contentobject_id AND
-                                                ezcontentobject_tree.contentobject_version = ezcontentobject_name.content_version AND ";
-                    $languageFilter = " AND " . eZContentLanguage::languagesSQLFilter( 'ezcontentobject' );
-                    $versionNameJoins .= eZContentLanguage::sqlFilter( 'ezcontentobject_name', 'ezcontentobject' );
-                    
-                    $rs = $db->arrayQuery( "SELECT ezkeyword.keyword
-                                            FROM ezkeyword,
-                                                ezkeyword_attribute_link,
-                                                ezcontentobject,
-                                                ezcontentobject_name,
-                                                ezcontentobject_attribute,
-                                                ezcontentobject_tree,
-                                                ezcontentclass
-                                                $sqlPermissionChecking[from]
-                                            WHERE ezkeyword.id = ezkeyword_attribute_link.keyword_id
-                                                AND ezkeyword_attribute_link.objectattribute_id = ezcontentobject_attribute.id
-                                                AND ezcontentobject_attribute.contentobject_id = ezcontentobject_tree.contentobject_id
-                                                AND ezkeyword.class_id = ezcontentclass.id
-                                                AND ezcontentclass.id = ezcontentobject.contentclass_id
-                                                AND ezcontentclass.version = '0'
-                                                AND ezcontentobject.status = '".eZContentObject::STATUS_PUBLISHED."'
-                                                AND ezcontentobject_attribute.version = ezcontentobject.current_version
-                                                AND ezcontentobject_tree.main_node_id = ezcontentobject_tree.node_id
-                                                AND ezcontentobject_attribute.contentobject_id = ezcontentobject.id
-                                                $pathString
-                                                $parentNodeIDSQL
-                                                $classIdentifierSQL
-                                                $showInvisibleNodesCond
-                                                $sqlPermissionChecking[where]
-                                                $languageFilter
-                                                $versionNameJoins
-                                            ORDER BY ezkeyword.keyword ASC" );
+                if( count( $tags ) != 0 )
+                {
+                    $maxCount = max( array_values( $tags ) );
+                    $minCount = min( array_values($tags ) );
+                }
 
-                    include_once ('lib/ezutils/classes/ezfunctionhandler.php');
-
-                    foreach( $rs as $row )
-                    {
-                        $tags[$row['keyword']] = eZFunctionHandler::execute( 'content', 'keyword_count', array( 'alphabet' => $row['keyword'], 
-                                                                                                                'strict_matching' => true,
-                                                                                                                'classid' => $classID  ) );
-                    }
-
-                    $maxFontSize = 200;
-                    $minFontSize = 100;
-
-                    $maxCount = 0;
-                    $minCount = 0;
-
-                    if( count( $tags ) != 0 )
-                    {
-                        $maxCount = max( array_values( $tags ) );
-                        $minCount = min( array_values($tags ) );
-                    }
-
-                    $spread = $maxCount - $minCount;
-                    if ( $spread == 0 )
+                $spread = $maxCount - $minCount;
+                if ( $spread == 0 )
                     $spread = 1;
 
-                    $step = ( $maxFontSize - $minFontSize )/( $spread );
+                $step = ( $maxFontSize - $minFontSize )/( $spread );
 
-                    foreach ($tags as $key => $value)
-                    {
-                        $size = $minFontSize + ( ( $value - $minCount ) * $step );
-                        $tagCloud[] = array( 'font_size' => $size,
-                                             'count' => $value,
-                                             'tag' => $key );
-                    }
+                foreach ($tags as $key => $value)
+                {
+                    $size = $minFontSize + ( ( $value - $minCount ) * $step );
+                    $tagCloud[] = array( 'font_size' => $size,
+                                         'count' => $value,
+                                         'tag' => $key );
+                }
 
-                    require_once( 'kernel/common/template.php' );
-                    $tpl = templateInit();
-                    $tpl->setVariable( 'tag_cloud', $tagCloud );
+                require_once( 'kernel/common/template.php' );
+                $tpl = templateInit();
+                $tpl->setVariable( 'tag_cloud', $tagCloud );
 
-                    $operatorValue = $tpl->fetch( 'design:tagcloud/tagcloud.tpl' );
-                } break;
+                $operatorValue = $tpl->fetch( 'design:tagcloud/tagcloud.tpl' );
+            } break;
         }
     }
 }
