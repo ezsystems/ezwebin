b399530403b4ac196961c5546803a0ac5303db35
Nicolas Pastorino <nfrp@ez.no>
2009-03-17 14:35:38 +0000

    --committed translations to esl-ES and cat-ES, received from Microblau
    git-svn-id: http://svn.ez.no/svn/extensions/ezwebin/trunk@3722 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- packages/ezwebin_extension/ezextension/ezwebin/translations/cat-ES/translation.ts
+++ packages/ezwebin_extension/ezextension/ezwebin/translations/cat-ES/translation.ts
@@ -1,5 +1,43 @@
 <!DOCTYPE TS><TS>
 <context>
+    <name>design/ezwebin/websitetoolbar/objectstates</name>
+    <message>
+        <source>Object states for object</source>
+        <translation>Estat de l&apos;objecte</translation>
+    </message>
+    <message>
+        <source>Content object state group</source>
+        <translation>Grup d&apos;estat de l&apos;objecte</translation>
+    </message>
+    <message>
+        <source>Available states</source>
+        <translation>Estats disponibles</translation>
+    </message>
+    <message>
+        <source>No content object state is configured. This can be done %urlstart here %urlend.</source>
+        <translation>Cap estat del objecte de contingut està configurat. Això, pot ser fet %urlstart aquí %urlend.</translation>
+    </message>
+    <message>
+        <source>Set states</source>
+        <translation>Establir estats</translation>
+    </message>
+    <message>
+        <source>Apply states from the list above.</source>
+        <translation>Aplica els estats en el llistat d&apos;adalt.</translation>
+    </message>
+    <message>
+        <source>No state to be applied to this content object. You might need to be assigned a more permissive access policy.</source>
+        <translation>Cap estat està aplicat a aquest objecte de contingut. És necessari assignar una política més permissiva.</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezwebin/parts/website_toolbar</name>
+    <message>
+        <source>Edit object states</source>
+        <translation>Editar estats de l&apos;objecte</translation>
+    </message>
+</context>
+<context>
     <name>design/ezwebin/article/article_index</name>
     <message>
         <source>Article index</source>
@@ -479,7 +517,7 @@
     </message>
     <message>
         <source>Documentation</source>
-        <translation>Documentació</translation>
+        <translation type="obsolete">Documentació</translation>
     </message>
     <message>
         <source>Translating content from %from_lang to %to_lang</source>
@@ -1732,6 +1770,14 @@ Les imatges es col·loquen a la biblioteca multimèdia per a poder ser reutilitz
         <source>Search</source>
         <translation>Cerca</translation>
     </message>
+    <message>
+        <source>Skip to main content</source>
+        <translation>Tornar al contingut principal</translation>
+    </message>
+    <message>
+        <source>Search text:</source>
+        <translation>Cercar texte:</translation>
+    </message>
 </context>
 <context>
     <name>design/ezwebin/parts/website_toolbar</name>
@@ -1774,6 +1820,10 @@ Les imatges es col·loquen a la biblioteca multimèdia per a poder ser reutilitz
     <message>
         <source>Import</source>
         <translation>Importa</translation>
+    </message>
+    <message>
+        <source>Sorting</source>
+        <translation>Ordenació</translation>
     </message>
 </context>
 <context>
@@ -2534,6 +2584,121 @@ Les imatges es col·loquen a la biblioteca multimèdia per a poder ser reutilitz
     <message>
         <source>Site map</source>
         <translation>Mapa del lloc</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezwebin/websitetoolbar/sort</name>
+    <message>
+        <source>Sub items [%children_count]</source>
+        <translation>Sub elements [%children_count]</translation>
+    </message>
+    <message>
+        <source>Show 10 items per page.</source>
+        <translation>Mostrar 10 elements per pàgina.</translation>
+    </message>
+    <message>
+        <source>Show 50 items per page.</source>
+        <translation>Mostrar 50 elements per pàgina.</translation>
+    </message>
+    <message>
+        <source>Show 25 items per page.</source>
+        <translation>Mostrar 25 elements per pàgina.</translation>
+    </message>
+    <message>
+        <source>Priority</source>
+        <translation>Prioritat</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Tipus</translation>
+    </message>
+    <message>
+        <source>Modifier</source>
+        <translation>Modificador</translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation>Modificat</translation>
+    </message>
+    <message>
+        <source>Section</source>
+        <translation>Secció</translation>
+    </message>
+    <message>
+        <source>Use the priority fields to control the order in which the items appear. You can use both positive and negative integers. Click the &quot;Update priorities&quot; button to apply the changes.</source>
+        <translation>Utilitza els camps de prioritat per controlar l&apos;ordre d&apos;aparició dels elements. Es pot utilitzar nombres enters positius i negatius. Prem el botò de &quot;Actualitzar prioritats&quot; per aplicar els canvis.</translation>
+    </message>
+    <message>
+        <source>You are not allowed to update the priorities because you do not have permission to edit &lt;%node_name&gt;.</source>
+        <translation>No està autoritzat per actuaitzar les prioritats degut a que no té permisos per editar &lt;%node_name&gt;.</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Desconegut</translation>
+    </message>
+    <message>
+        <source>The current item does not contain any sub items.</source>
+        <translation>L&apos;element actual no conté cap sub element.</translation>
+    </message>
+    <message>
+        <source>Update priorities</source>
+        <translation>Actualitzar prioritats</translation>
+    </message>
+    <message>
+        <source>Apply changes to the priorities of the items in the list above.</source>
+        <translation>Aplicar canvis a les prioritats del elements en la llista de dalt.</translation>
+    </message>
+    <message>
+        <source>You cannot update the priorities because you do not have permission to edit the current item or because a non-priority sorting method is used.</source>
+        <translation>No pot actualitzar les prioritats degut a que no té permisos per editar l&apos;element actual o degut a que no s&apos;està utilitzant una ordenació per prioritats.</translation>
+    </message>
+    <message>
+        <source>Sorting</source>
+        <translation>Ordenació</translation>
+    </message>
+    <message>
+        <source>Class identifier</source>
+        <translation>Identificador de classe</translation>
+    </message>
+    <message>
+        <source>Class name</source>
+        <translation>Nom de la classe</translation>
+    </message>
+    <message>
+        <source>Depth</source>
+        <translation>Profunditat</translation>
+    </message>
+    <message>
+        <source>Published</source>
+        <translation>Publicat</translation>
+    </message>
+    <message>
+        <source>You cannot set the sorting method for the current location because you do not have permission to edit the current item.</source>
+        <translation>No pot establir el mètode d&apos;ordenació de la ubicació actual degut a que no té permisos  per editar l&apos;element actual.</translation>
+    </message>
+    <message>
+        <source>Use these controls to set the sorting method for the sub items of the current location.</source>
+        <translation>Utilitza aquest controls per establir un mètode d&apos;ordenació per als sub elements a la ubicació actual.</translation>
+    </message>
+    <message>
+        <source>Descending</source>
+        <translation>Descendent</translation>
+    </message>
+    <message>
+        <source>Ascending</source>
+        <translation>Ascendent</translation>
+    </message>
+    <message>
+        <source>Set</source>
+        <translation>Establir</translation>
+    </message>
+    <message>
+        <source>Invalid or missing parameter: %parameter</source>
+        <translation>Paràmetre invàlid o no introduït: %parameter</translation>
     </message>
 </context>
 </TS>
--- packages/ezwebin_extension/ezextension/ezwebin/translations/esl-ES/translation.ts
+++ packages/ezwebin_extension/ezextension/ezwebin/translations/esl-ES/translation.ts
@@ -1,5 +1,43 @@
 <!DOCTYPE TS><TS>
 <context>
+    <name>design/ezwebin/websitetoolbar/objectstates</name>
+    <message>
+        <source>Object states for object</source>
+        <translation>Estados de los objetos</translation>
+    </message>
+    <message>
+        <source>Content object state group</source>
+        <translation>Grupo de estado del objeto</translation>
+    </message>
+    <message>
+        <source>Available states</source>
+        <translation>Estados disponibles</translation>
+    </message>
+    <message>
+        <source>No content object state is configured. This can be done %urlstart here %urlend.</source>
+        <translation>No está configurador el estado del objeto de contenido. Puede hacerse %urlstart aquí %urlend.</translation>
+    </message>
+    <message>
+        <source>Set states</source>
+        <translation>Establecer estados</translation>
+    </message>
+    <message>
+        <source>Apply states from the list above.</source>
+        <translation>Aplicar estados de la lista de arriba.</translation>
+    </message>
+    <message>
+        <source>No state to be applied to this content object. You might need to be assigned a more permissive access policy.</source>
+        <translation>Este objeto de contendio no tiene aplicado un estado. Necesitas ser asignado en una política de acceso más permisiva.</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezwebin/parts/website_toolbar</name>
+    <message>
+        <source>Edit object states</source>
+        <translation>Editar estado del objeto</translation>
+    </message>
+</context>
+<context>
     <name>design/ezwebin/article/article_index</name>
     <message>
         <source>Article index</source>
@@ -479,7 +517,7 @@
     </message>
     <message>
         <source>Documentation</source>
-        <translation>Documentación</translation>
+        <translation type="obsolete">Documentación</translation>
     </message>
     <message>
         <source>Translating content from %from_lang to %to_lang</source>
@@ -1566,6 +1604,13 @@ Las imágenes se colocan en la biblioteca multimedia para poder ser reutilizadas
     </message>
 </context>
 <context>
+    <name>design/ezwebin/line/silverlight</name>
+    <message>
+        <source>View</source>
+        <translation>Ver</translation>
+    </message>
+</context>
+<context>
     <name>design/ezwebin/line/windows_media</name>
     <message>
         <source>View movie</source>
@@ -1728,6 +1773,14 @@ Las imágenes se colocan en la biblioteca multimedia para poder ser reutilizadas
         <source>Search</source>
         <translation>Buscar</translation>
     </message>
+    <message>
+        <source>Skip to main content</source>
+        <translation>Volver al contenido principal</translation>
+    </message>
+    <message>
+        <source>Search text:</source>
+        <translation>Buscar texto:</translation>
+    </message>
 </context>
 <context>
     <name>design/ezwebin/parts/website_toolbar</name>
@@ -1770,6 +1823,10 @@ Las imágenes se colocan en la biblioteca multimedia para poder ser reutilizadas
     <message>
         <source>Import</source>
         <translation>Importar</translation>
+    </message>
+    <message>
+        <source>Sorting</source>
+        <translation>Ordenación</translation>
     </message>
 </context>
 <context>
@@ -2557,6 +2614,121 @@ Las imágenes se colocan en la biblioteca multimedia para poder ser reutilizadas
     <message>
         <source>Site map</source>
         <translation>Mapa web</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezwebin/websitetoolbar/sort</name>
+    <message>
+        <source>Sub items [%children_count]</source>
+        <translation>Sub elementos [%children_count]</translation>
+    </message>
+    <message>
+        <source>Show 10 items per page.</source>
+        <translation>Mostrar 10 elementos por página.</translation>
+    </message>
+    <message>
+        <source>Show 50 items per page.</source>
+        <translation>Mostrar 50 elementos por página.</translation>
+    </message>
+    <message>
+        <source>Show 25 items per page.</source>
+        <translation>Mostrar 25 elementos por página.</translation>
+    </message>
+    <message>
+        <source>Priority</source>
+        <translation>Prioridad</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Tipo</translation>
+    </message>
+    <message>
+        <source>Modifier</source>
+        <translation>Modificador</translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation>Modificado</translation>
+    </message>
+    <message>
+        <source>Section</source>
+        <translation>Sección</translation>
+    </message>
+    <message>
+        <source>Use the priority fields to control the order in which the items appear. You can use both positive and negative integers. Click the &quot;Update priorities&quot; button to apply the changes.</source>
+        <translation>Utiliza los campos de prioridad para controlar el orden en el que aparecen los elementos. Puede usar números enteros positivos y negativos. Persiona el botón &quot;Actualizar prioridades&quot; para aplicar los cambios.</translation>
+    </message>
+    <message>
+        <source>You are not allowed to update the priorities because you do not have permission to edit &lt;%node_name&gt;.</source>
+        <translation>No está autorizado para actualizar las prioridades debido a que no tiene permisos de edición &lt;%node_name&gt;.</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Desconocido</translation>
+    </message>
+    <message>
+        <source>The current item does not contain any sub items.</source>
+        <translation>El elemento actual no contiene ningún sub elemento.</translation>
+    </message>
+    <message>
+        <source>Update priorities</source>
+        <translation>Actualizar prioridades</translation>
+    </message>
+    <message>
+        <source>Apply changes to the priorities of the items in the list above.</source>
+        <translation>Aplicar cambios a las prioridades de los elementos de la lista de arriba.</translation>
+    </message>
+    <message>
+        <source>You cannot update the priorities because you do not have permission to edit the current item or because a non-priority sorting method is used.</source>
+        <translation>No puede actualizar las prioridades debido a que no tiene permiso para editar el elemento actual o porque no esta utilizando un método de ordenación por prioridades.</translation>
+    </message>
+    <message>
+        <source>Sorting</source>
+        <translation>Ordenación</translation>
+    </message>
+    <message>
+        <source>Class identifier</source>
+        <translation>Identificador de clase</translation>
+    </message>
+    <message>
+        <source>Class name</source>
+        <translation>Nombre de la clase</translation>
+    </message>
+    <message>
+        <source>Depth</source>
+        <translation>Profundidad</translation>
+    </message>
+    <message>
+        <source>Published</source>
+        <translation>Publicado</translation>
+    </message>
+    <message>
+        <source>You cannot set the sorting method for the current location because you do not have permission to edit the current item.</source>
+        <translation>No puede establecer el método de ordenación de la ubicación actual debido a que no tiene permisos para editar el elemento actual.</translation>
+    </message>
+    <message>
+        <source>Use these controls to set the sorting method for the sub items of the current location.</source>
+        <translation>Utiliza estos controles para establecer el método de ordenación para los sub elementos de la ubicación actual.</translation>
+    </message>
+    <message>
+        <source>Descending</source>
+        <translation>Descendente</translation>
+    </message>
+    <message>
+        <source>Ascending</source>
+        <translation>Ascendente</translation>
+    </message>
+    <message>
+        <source>Set</source>
+        <translation>Establecer</translation>
+    </message>
+    <message>
+        <source>Invalid or missing parameter: %parameter</source>
+        <translation>Parametro inválido o no introducido: %parameter</translation>
     </message>
 </context>
 </TS>
