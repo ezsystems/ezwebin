2d72c91c8eea1cc622ab908730fd76cc4d4ef0e1
Ole Marius Smestad <oms@ez.no>
2009-03-17 15:12:58 +0000

    - Updated: translation.
    git-svn-id: http://svn.ez.no/svn/extensions/ezwebin/trunk@3730 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- packages/ezwebin_extension/ezextension/ezwebin/translations/ita-IT/translation.ts
+++ packages/ezwebin_extension/ezextension/ezwebin/translations/ita-IT/translation.ts
@@ -1,5 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="it_IT">
+<!DOCTYPE TS><TS>
 <defaultcodec></defaultcodec>
 <context>
     <name>design/ezwebin/article/article_index</name>
