cc4558e5fce38092959f300dbd032195fce14622
André Rømcke <ar@ez.no>
2010-02-25 16:09:01 +0000

    - Fixed bug #16262: User preferences right menu item not enabled in 4.3.0beta1
    git-svn-id: http://svn.ez.no/svn/extensions/ezwebin/trunk@4447 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- packages/ezwebin_site/settings/ezwebininstaller.php
+++ packages/ezwebin_site/settings/ezwebininstaller.php
@@ -1760,8 +1760,9 @@ class eZWebinInstaller extends eZSiteInstaller
                 ), 
                 'Toolbar_admin_right' => array( 
                     'Tool' => array( 
-                        0 => 'admin_current_user', 
-                        1 => 'admin_bookmarks' 
+                        0 => 'admin_current_user',
+                        1 => 'admin_preferences',
+                        2 => 'admin_bookmarks'
                     ) 
                 ), 
                 'Toolbar_admin_developer' => array( 
